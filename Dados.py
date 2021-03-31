0071058fd894dee893e2d23e9ff03215a2747633
Will Lima <limawill83@gmail.com>
2021-03-31 10:18:26 -0300

    Update Dados.py

--- Dados.py
+++ Dados.py
@@ -1,49 +1,112 @@
-import pandas as pd
-import json
-import pprint 
+# We import Flask
+from flask import Flask, jsonify, request, render_template, Blueprint
+from flask_paginate import Pagination, get_page_args, get_page_parameter
 import urllib.request
-import requests
-import sys
+import json
+    
+# We create a Flask app
+app = Flask(__name__)
+app.template_folder = ''
+users = list(range(10))
 
 
-# Variavél que recebe a massa de dados
-url = "http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-2.json"
+#variaveis globais
+#urlData = "http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-sample.json"
+urlData = "http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-2.json"
 
-
-#Função responsável pela abertura e verificação de acesso aos dados
-def ler_json(arq_json):
-    acessoPagina = requests.get(arq_json)
-    if acessoPagina.status_code >= 200:
-        with urllib.request.urlopen(arq_json) as response:
-            return response.read()
-    else:
-        print("Erro de acesso aos dados")
-        sys.exit([acessoPagina])
-
-
-data = ler_json(url)
+#Bounding Box Grupo ZAP
+minLon = -46.693419
+minLat = -23.568704
+maxLon = -46.641146
+maxLat = -23.546686
 
 #Listas de armazenamento
-listCasa1 = []
-listCasa2 = []
+listZap = []
+listViva = []
+listExcluidas = []
 
-
-
-for item in data:
-
-    tipoManga = item['pricingInfos']['businessType']
-    price = int(item['pricingInfos']['price'])
-    longitude = item['address']['geoLocation']['location']['lon']
-    latitude = item['address']['geoLocation']['location']['lat']
-
-    if longitude != 0 and latitude !=0:
-      if tipoManga == 'RENTAL' and price >= 3500 or tipoManga == 'SALE' and price >= 600000:
-        listCasa1.append(item)
-
-        if tipoManga == 'RENTAL' and price >= 4000 or tipoManga == 'SALE' and price >= 700.000:
-          listCasa2.append(item)
+def getResponse(url):
+    operUrl = urllib.request.urlopen(url)
+    if(operUrl.getcode()==200):
+        data = operUrl.read()
+        jsonData = json.loads(data)
     else:
-      print ("Temos 1")
+        print("Error receiving data", operUrl.getcode())
+    return jsonData
 
-print(listCasa1)
-print(listCasa2)
+
+def escrever_json(lista):
+    with open('meu_arquivo.json', 'w') as f:
+        json.dump(lista, f)
+
+
+    
+
+@app.route('/vivareal', methods=['GET'])
+def vivareal():
+    jsonData = getResponse(urlData)
+    count = 0
+    
+    print(f'Tamanho do jsonData: ',len(jsonData))
+    for i in jsonData:
+        #Valores capturados para domada de decisão
+        valorImovel = int(i["pricingInfos"]["price"])
+        tipoNegocio = i["pricingInfos"]["businessType"]
+        longitude = i["address"]["geoLocation"]["location"]["lon"]
+        latitude = i["address"]["geoLocation"]["location"]["lat"]
+        areaUsada = i["usableAreas"]
+    
+        #Quando o imovel é venda ou falta o campo condominio
+        try:
+            condoFree  = int(i["pricingInfos"]["monthlyCondoFee"])
+        except NameError as error:
+            print(f'Dentro do except')
+        except KeyError:
+            condoFree = 0
+        
+         #Variavel de contagem    
+        count += 1
+
+        #Verificando se está nas proximidades da OLX
+        if minLon <= longitude and maxLon >= longitude and minLat <= latitude and maxLat >= latitude:
+            i["pricingInfos"]["price"] = valorImovel + (valorImovel * 0.5)
+
+        if longitude == 0 and latitude == 0:
+            listExcluidas.append(i)
+        else:
+            if tipoNegocio == 'RENTAL' and valorImovel >= 4000 or tipoNegocio == 'SALE' and valorImovel >= 700000:
+                listViva.append(i)
+            
+    totalList = len(listViva)
+    print(f'Tamanho da lista: ',totalList)
+    escrever_json(listViva)
+    #Inicio da paginação
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    pagination = Pagination(page=page, total=totalList, search=True, record_name='users')
+    return render_template('index.html',
+                           page=page,
+                           users=listViva, # O List com as informações
+                           per_page=10,
+                           offset=0,
+                           pagination=pagination,
+                           )
+
+
+
+
+
+
+# We establish a Flask route so that we can serve HTTP traffic on that route 
+@app.route('/')
+def weather():
+    return 'Olá Mundo! - ZAP'
+
+@app.route("/zap", methods=['GET'])
+def zap():
+    return 'Olá Mundo! - ZAP'
+
+# Get setup so that if we call the app directly (and it isn't being imported elsewhere)
+# it will then run the app with the debug mode as True
+# More info - https://docs.python.org/3/library/__main__.html
+if __name__ == '__main__':
+    app.run(debug=True)
